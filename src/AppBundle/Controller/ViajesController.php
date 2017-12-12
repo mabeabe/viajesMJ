e79992ecac4219d296a47cebe814230e093ad9f7
mabeabe <mabeabe@gmail.com>
2017-12-12 20:41:46 +0100

    rutas

--- src/AppBundle/Controller/ViajesController.php
+++ src/AppBundle/Controller/ViajesController.php
@@ -6,17 +6,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class ViajesController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     *@Route("/viajes/europa")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('viajes/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
-      
     }
 }
